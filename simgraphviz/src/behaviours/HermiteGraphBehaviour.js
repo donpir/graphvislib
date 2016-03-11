b3f77f6a35b8bd75fd9d52e149279663b0c7d5f1
Donato <donatopirozzi@gmail.com>
2016-03-11 17:25:13 +0100

    Started the implementation of the graph with hermite curves

--- simgraphviz/src/behaviours/HermiteGraphBehaviour.js
+++ simgraphviz/src/behaviours/HermiteGraphBehaviour.js
@@ -19,9 +19,12 @@ HermiteGraphBehaviour.prototype.init = function(svg, jsonGraph, force) {
     this.linkShapes = this._svg.selectAll(".lnkShapes")
         .data(this._jsonGraph.links).enter();
 
-    this.link = this.linkShapes
+    /*this.link = this.linkShapes
         .append("line")
-        //.filter(function (d) { return (typeof d.type == 'marker' && d.ends.length > 0); })
+        .attr("class", "link");*/
+
+    this.link = this._
+        .append("g")
         .attr("class", "link");
 
     //NODES.
@@ -38,6 +41,41 @@ HermiteGraphBehaviour.prototype.init = function(svg, jsonGraph, force) {
         .attr("class", "nodeTitle")
         .text(function(d) { return d.name; });
 };
+
+var n=4,
+    q=[{"i":0,"x":100,"y":150},{"i":1,"x":200,"y":80},{"i":2,"x":300,"y":250},{"i":3,"x":350,"y":100}],
+    steps=100;
+
+
+var line = d3.svg.line()
+    .x(function(d) {return d.x;})
+    .y(function(d) {return d.y;})
+    .interpolate("linear");
+
+function hermite(t) {
+    var q0x=q[0].x,
+        q0y=q[0].y,
+
+        q1x=q[1].x-q[0].x,
+        q1y=q[1].y-q[0].y,
+
+        q2x=q[2].x,
+        q2y=q[2].y,
+
+        q3x=q[3].x-q[2].x,
+        q3y=q[3].y-q[2].y;
+
+    tt=Math.pow(t,2);
+    ttt=Math.pow(t,3);
+    return {"x": (2*ttt-3*tt+1)*q0x+(ttt-2*tt+t)*q1x+(-2*ttt+3*tt)*q2x+(ttt-tt)*q3x,
+        "y": (2*ttt-3*tt+1)*q0y+(ttt-2*tt+t)*q1y+(-2*ttt+3*tt)*q2y+(ttt-tt)*q3y};
+}
+
+function makeCurve() {
+    var curve = d3.range(steps+1).map(function(i) { return hermite(i/steps);})
+    return [curve];
+}
+
 
 HermiteGraphBehaviour.prototype.update = function() {
     var _this = this;
@@ -56,11 +94,18 @@ HermiteGraphBehaviour.prototype.update = function() {
         .attr("y", function(d) { return d.y - 3; });
 
     this.link
-        .attr("x1", function(d) { return d.source.x + ((d.source.x < d.target.x) ? nodeWidth : 0); })
+        .data(makeCurve())
+        .enter()
+        .append("path")
+        .attr("d", line);
+
+
+        /*.attr("x1", function(d) { return d.source.x + ((d.source.x < d.target.x) ? nodeWidth : 0); })
         .attr("y1", function(d) { return d.source.y + d.sourceStart / reducer; })
         .attr("x2", function(d) { return d.target.x + ((d.source.x < d.target.x) ? 0 : nodeWidth); })
         .attr("y2", function(d) { return d.target.y + d.targetStart / reducer; })
         .style("stroke", function(d) { return d.group == 1 ? col1: col2; })
-        .style("stroke-width", function(d) { return d.length / reducer; });
+        .style("stroke-opacity", function(d) { return d.group == 1 ? .6 : .3; })
+        .style("stroke-width", function(d) { return d.length / reducer; });*/
 
 };
